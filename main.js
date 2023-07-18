9fe9f75a21138b7d3cdd0c42fe56f124dbf0eba3
rock-biter <74297920+rock-biter@users.noreply.github.com>
2023-07-18 16:07:43 +0200

    EDIT - geodetic curves

--- main.js
+++ main.js
@@ -1,39 +1,77 @@
 import './style.css'
 import * as THREE from 'three'
-import { ArcballControls } from 'three/examples/jsm/controls/ArcballControls'
-import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls'
-import { FlyControls } from 'three/examples/jsm/controls/FlyControls'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { MapControls } from 'three/examples/jsm/controls/MapControls'
-import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls'
-import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls'
+import map from './textures/water.png?url'
+import cloudsMap from './textures/clouds.jpg?url'
+
+const textureLoader = new THREE.TextureLoader()
+
+const locations = [
+	{
+		name: 'Lisbona',
+		coords: [38.74, -9.2],
+	},
+	{
+		name: 'Syros',
+		coords: [37.43, 24.87],
+	},
+	{
+		name: 'Valfrejus',
+		coords: [45.17, 6.64],
+	},
+	{
+		name: 'Albenga',
+		coords: [44.05, 8.19],
+	},
+	{
+		name: 'Salerno',
+		coords: [40.67, 14.75],
+	},
+	{
+		name: 'Milano',
+		coords: [45.46, 9.09],
+	},
+	{
+		name: 'Torino',
+		coords: [45.07, 7.63],
+	},
+	{
+		name: 'Hội An',
+		coords: [15.91, 108.33],
+	},
+	{
+		name: 'Ko Lanta',
+		coords: [7.66, 98.92],
+	},
+	{
+		name: 'Kyoto',
+		coords: [35.09, 135.55],
+	},
+	{
+		name: 'Ko Samet',
+		coords: [12.55, 101.43],
+	},
+	{
+		name: 'Tokyo',
+		coords: [35.5, 139.11],
+	},
+	{
+		name: 'Hanoi',
+		coords: [21.02, 105.81],
+	},
+	{
+		name: 'Kuala Lumpur',
+		coords: [3.13, 101.6],
+	},
+]
 
 /**
  * Scene
  */
 const scene = new THREE.Scene()
 
-/**
- * Manhattan
- */
-const material = new THREE.MeshNormalMaterial()
-
-const size = 6
-
-for (let i = 0; i < size; i++) {
-	for (let j = 0; j < size; j++) {
-		const height = Math.random() * 4 + 1
-
-		const geometry = new THREE.BoxGeometry(1, height, 1)
-
-		const mesh = new THREE.Mesh(geometry, material)
-		mesh.position.set(-size + i * 2, height / 2, -size + j * 2)
-		scene.add(mesh)
-	}
-}
-
-const mouse = new THREE.Vector2(0, 0)
-let factor = 0
+const axesHelper = new THREE.AxesHelper(10)
+// scene.add(axesHelper)
 
 /**
  * render sizes
@@ -48,7 +86,7 @@ const sizes = {
 const fov = 90
 const camera = new THREE.PerspectiveCamera(fov, sizes.width / sizes.height, 0.1)
 
-camera.position.set(10, 0.5, 10)
+camera.position.set(7.5, 10, 7.5)
 camera.lookAt(new THREE.Vector3(0, 2.5, 0))
 
 /**
@@ -65,119 +103,58 @@ renderer.setPixelRatio(pixelRatio)
 document.body.appendChild(renderer.domElement)
 
 /**
- * muovo indietro la camera
- */
-// camera.position.z = 4
-
-/**
- * ArcballControls
- */
-
-// const controls = new ArcballControls(camera, renderer.domElement, scene)
-
-/**
- * FlyControls
- */
-// const controls = new FlyControls(camera, renderer.domElement)
-// controls.movementSpeed = 2
-// controls.rollSpeed = 0.5
-// controls.autoForward = true
-
-/**
- * FirstPersonControls
- */
-// const controls = new FirstPersonControls(camera, renderer.domElement)
-// controls.lookSpeed = 0.03
-
-// // impedisce inclinazione verticale
-// controls.lookVertical = false
-
-// limita gli angoli di inclinazione verticale
-// controls.verticalMin = Math.PI * 0.25
-// controls.verticalMax = Math.PI * 0.75
-// controls.constrainVertical = true
-// controls.heightSpeed = true
-// controls.heightMax = 100
-// controls.heightCoef = 3
-
-/**
  * OrbitControls
  */
-// const controls = new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
 
-/**
- * MapControls
- */
-// const controls = new MapControls(camera, renderer.domElement)
-// attiva la rotazione automatica
-// controls.autoRotate = true
-// imposta la velocità di rotazione
-// controls.autoRotateSpeed = 3.0
-// controls.enableRotate = false
-// controls.enableDamping = true
-// controls.dampingFactor = 0.07
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.7)
+const pointLight = new THREE.PointLight('#ffffff', 0.3, 50)
+scene.add(ambientLight)
+camera.add(pointLight)
+scene.add(camera)
 
-// controls.enablePan = false
-// controls.panSpeed = 2
-// controls.screenSpacePanning = false
-// controls.listenToKeyEvents(window)
-// controls.keys = {
-// 	LEFT: 'KeyA',
-// 	UP: 'KeyW',
-// 	RIGHT: 'KeyD',
-// 	BOTTOM: 'KeyS',
-// }
+const earth = createEarth()
+earth.rotation.y = Math.PI * -0.3
+scene.add(earth)
+const clouds = earth.getObjectByName('cloud')
 
-// controls.minDistance = 2
-// controls.maxDistance = 20
+let prevLocation
 
-// controls.minPolarAngle = Math.PI * 0.25
-// controls.maxPolarAngle = Math.PI * 0.75
+locations.forEach((loc, i) => {
+	const mesh = createLocation(loc, i)
+	loc.mesh = mesh
+	earth.add(mesh)
 
-// controls.minAzimuthAngle = -Math.PI * 0.5
-// controls.maxAzimuthAngle = Math.PI * 0.5
+	if (prevLocation) {
+		earth.add(createPath(prevLocation, loc))
+	}
 
-// /**
-//  * PointerLockControls
-//  */
-// const controls = new PointerLockControls(camera, document.body)
-
-/**
- * PointerLockControls
- */
-const controls = new TrackballControls(camera, renderer.domElement)
-controls.dynamicDampingFactor = 0.05
+	prevLocation = loc
+})
 
 /**
  * velocità di rotazione radianti al secondo
  */
-const vel = new THREE.Vector3(0, 0, 0)
+const vel = {
+	value: 0.006,
+}
 
-/**
- * Three js Clock
- */
 const clock = new THREE.Clock()
 
 /**
  * frame loop
  */
 function tic() {
-	/**
-	 * tempo trascorso dal frame precedente
-	 */
-	const deltaTime = clock.getDelta()
-	/**
-	 * tempo totale trascorso dall'inizio
-	 */
-	// const time = clock.getElapsedTime()
 	controls.update()
-
-	if (controls.isLocked) {
-		controls.moveForward(vel.z * deltaTime)
-		controls.moveRight(vel.x * deltaTime)
-	}
+	const delta = clock.getDelta()
 
 	renderer.render(scene, camera)
+
+	earth.rotation.y -= delta * vel.value * 5
+	if (clouds) {
+		clouds.rotation.y += delta * vel.value
+	}
 
 	requestAnimationFrame(tic)
 }
@@ -198,61 +175,140 @@ function onResize() {
 
 	const pixelRatio = Math.min(window.devicePixelRatio, 2)
 	renderer.setPixelRatio(pixelRatio)
-
-	controls.handleResize()
 }
 
-controls.addEventListener('lock', function () {
-	// menu.style.display = 'none'
-})
+function createEarth() {
+	const mapTexture = textureLoader.load(map)
 
-controls.addEventListener('unlock', function () {
-	// menu.style.display = 'block'
-})
+	const geometry = new THREE.SphereGeometry(8, 90, 90)
+	const material = new THREE.MeshStandardMaterial({
+		color: 0x88af34,
+		transparent: true,
+		// opacity: 0.5,
+		map: mapTexture,
+	})
 
-// window.addEventListener('click', function () {
-// 	// cliccando sulla window attiviamo il controls se non è attivo
-// 	if (!controls.isLocked) {
-// 		controls.lock()
-// 	}
-// })
+	material.onBeforeCompile = (shader) => {
+		// console.log(shader.fragmentShader)
+		shader.fragmentShader = shader.fragmentShader.replace(
+			'#include <map_fragment>',
+			`
+		#ifdef USE_MAP
+			vec4 map = texture2D( map, vMapUv );
+			diffuseColor *= vec4( vec3(1.) - map.rgb, 1. - map.r );
 
-window.addEventListener('keydown', function (e) {
-	// disattivo il controls se l'utente preme il tasto Esc
-	switch (e.code) {
-		case 'KeyA':
-		case 'ArrowLeft':
-			vel.x = -1
-			break
-		case 'KeyD':
-		case 'ArrowRight':
-			vel.x = 1
-			break
-		case 'KeyW':
-		case 'ArrowUp':
-			vel.z = 1
-			break
-		case 'KeyS':
-		case 'ArrowDown':
-			vel.z = -1
-			break
+		#endif
+		`
+		)
 	}
-})
 
-window.addEventListener('keyup', function (e) {
-	// disattivo il controls se l'utente preme il tasto Esc
-	switch (e.code) {
-		case 'KeyA':
-		case 'ArrowLeft':
-		case 'KeyD':
-		case 'ArrowRight':
-			vel.x = 0
-			break
-		case 'KeyW':
-		case 'ArrowUp':
-		case 'KeyS':
-		case 'ArrowDown':
-			vel.z = 0
-			break
+	const atmo = createAtmo()
+	const water = createWater(mapTexture)
+	const earth = new THREE.Mesh(geometry, material)
+
+	const clouds = createClouds()
+
+	const group = new THREE.Group()
+	group.add(atmo, clouds, earth, water)
+
+	return group
+}
+
+scene.background = new THREE.Color('#ffcc33')
+
+function createWater(map) {
+	const geometry = new THREE.SphereGeometry(7.85, 90, 90)
+	const material = new THREE.MeshStandardMaterial({
+		color: 0x38bdf8,
+		map: map,
+	})
+	const water = new THREE.Mesh(geometry, material)
+	water.name = 'water'
+
+	return water
+}
+
+function createAtmo() {
+	const geometry = new THREE.SphereGeometry(8.15, 90, 90)
+	const material = new THREE.MeshStandardMaterial({
+		color: 0x000000,
+		transparent: true,
+		// opacity: 0.9,
+		side: THREE.BackSide,
+		depthWrite: false,
+	})
+
+	const atmo = new THREE.Mesh(geometry, material)
+	atmo.name = 'atmo'
+	return atmo
+}
+
+function createClouds() {
+	const geometry = new THREE.SphereGeometry(8.2, 90, 90)
+	const map = textureLoader.load(cloudsMap)
+	const material = new THREE.MeshStandardMaterial({
+		color: 0xffffff,
+		transparent: true,
+		alphaMap: map,
+	})
+
+	const clouds = new THREE.Mesh(geometry, material)
+	clouds.name = 'cloud'
+	return clouds
+}
+
+function createLocation(location, i) {
+	const material = new THREE.MeshBasicMaterial({
+		color: 0xffffff,
+	})
+
+	const geometry = new THREE.CylinderGeometry(0.1, 0.1, 0.01)
+	const mesh = new THREE.Mesh(geometry, material)
+
+	const [lt, ln] = location.coords
+
+	const pos = getLocPositionByCoords(lt, ln)
+	mesh.position.copy(pos)
+
+	mesh.lookAt(0, 0, 0)
+	mesh.geometry.rotateX(Math.PI * 0.5)
+
+	return mesh
+}
+
+function getLocPositionByCoords(lt, ln, radius = 8.01) {
+	const t = THREE.MathUtils.degToRad(90 - lt)
+	const p = THREE.MathUtils.degToRad(ln + 90)
+
+	const pos = new THREE.Vector3()
+	pos.setFromSphericalCoords(radius, t, p)
+
+	return pos
+}
+
+function createPath(locA, locB) {
+	const A = getLocPositionByCoords(...locA.coords, 8)
+	const B = getLocPositionByCoords(...locB.coords, 8)
+
+	const points = []
+	const n = 10
+
+	for (let i = 0; i < n + 1; i++) {
+		const point = A.clone()
+			.lerp(B, 0.1 * i)
+			.normalize()
+			.multiplyScalar(8 + Math.sin((Math.PI * i) / 10) * 2)
+		points.push(point)
 	}
-})
+
+	const curve = new THREE.CatmullRomCurve3(points)
+
+	const geometry = new THREE.TubeGeometry(curve, 50, 0.02, 5)
+	const material = new THREE.MeshBasicMaterial({
+		color: 0xffffff,
+	})
+
+	const tube = new THREE.Mesh(geometry, material)
+
+	return tube
+}
